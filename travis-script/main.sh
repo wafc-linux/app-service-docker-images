4570affa5b00c64803f66ad61e5cb1d940155841
sunitha muthukrishna <sumuth@microsoft.com>
2018-01-30 13:28:56 -0800

    updated travis script

--- travis-script/main.sh
+++ travis-script/main.sh
@@ -199,13 +199,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo $TRAVIS_COMMIT
     echo $TRAVIS_COMMIT >> result.log
     SignOff="#sign-off"        
-    sign0ff=$(echo "${TRAVIS_COMMIT_MESSAGE}" | grep "${SignOff}")
-    if [ -n "${signoff}" ]; then
+    signOff=$(echo "${TRAVIS_COMMIT_MESSAGE}" | grep "${SignOff}")
+    if [ -n "${signOff}" ]; then
         echo "========================================"
         echo "========================================" >> result.log
         echo "INFORMATION - COMMIT MESSAGE contains #sign-off, using PROD......"
         echo "INFORMATION - COMMIT MESSAGE contains #sign-off, using PROD......" >> result.log
         DOCKER_USERNAME=$PROD_DOCKER_USERNAME
+        DOCKER_ACCOUNT=$PROD_DOCKER_ACCOUNT
         DOCKER_PASSWORD=$PROD_DOCKER_PASSWORD
     fi    
     commit_sha=$TRAVIS_COMMIT    
